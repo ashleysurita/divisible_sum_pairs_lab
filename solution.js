f471ac3474f17e590f2ff195b22ed125240cbefd
Ashley <ashleywong678@gmail.com>
2020-02-22 14:45:27 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,20 @@
+// Complete the divisibleSumPairs function below.
+
+//pseudocode
+// n = size of array
+// k = what the sum of ar numbers need to be evenly divisible by
+// ar = array of numbers to check the sum
+
+
+function divisibleSumPairs(n, k, ar) {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = (i+1); j < n; j++) {
+            if ((ar[i] + ar[j]) % k == 0) {
+                sum++;
+                console.log(ar[i],ar[j]);
+            }
+        }
+    }
+    return sum;
+}
